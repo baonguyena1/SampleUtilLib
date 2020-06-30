ebd6b1d35b89991679c36b58297189d836fc74c0
Bao Nguyen <baon@tech.est-rouge.com>
2020-06-30 11:18:31 +0700

    update script

--- release.sh
+++ release.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
+set -e
+underline=$(tput smul)
+nounderline=$(tput rmul)
 
 WORKSPACE="$( cd "$(dirname ${BASH_SOURCE[0]})" >/dev/null 2>& 1 && pwd )/"
 
@@ -11,9 +14,33 @@ function checkout_source() {
     echo "➡️ Checkout Source"
     cd $WORKSPACE
     git reset --hard
-    git checkout master
+    git checkout $SOURCE_BRANCH
     git fetch
     git pull
 }
 
+function usage() {
+    cat <<EOF
+$(underline)Usage: $0 options
+
+$(underline)options:
+    -v  SDK version (require).
+EOF
+}
+
+function check_commands() {
+    while getopts v: option
+    do
+    case "$option"
+    in
+    v) SDK_VERSION=$(OPTARG);;
+    esac
+    done
+    if [ $SDK_VERSION == "" ]; then
+        usage
+        exit 1
+    fi
+}
+
+check_commands
 checkout_source
